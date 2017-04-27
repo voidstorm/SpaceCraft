6dcbeb2178107beff491444caa7403a7badce58f
Florian Putz <florian.putz@igt.com>
2017-04-27 15:25:36 +0200

    added FindGLM.cmake

--- vortexcore/cmake/FindGLM.cmake
+++ vortexcore/cmake/FindGLM.cmake
@@ -9,12 +9,7 @@
 #
 #
 
-include(FindPackageHandleStandardArgs)
-include(FindLibraryDebugRelease)
-
 find_path(GLM_INCLUDE_DIR glm/glm.hpp)
-#find_library_debug_release(GLFW_LIBRARY "glfw3;glfw")
 SET(GLM_LIBRARY "")
 
-find_package_handle_standard_args(GLFW DEFAULT_MSG GLM_INCLUDE_DIR GLM_LIBRARY)
 mark_as_advanced(GLM_INCLUDE_DIR GLM_LIBRARY)
