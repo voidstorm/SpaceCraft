01ee5f147172bc13f0d86b481721ce3564f122cf
voidstorm <flo.putz@gmail.com>
2017-05-01 22:36:25 +0200

    fixed update script

--- spacecraft/update_dlls.bat
+++ spacecraft/update_dlls.bat
@@ -1,8 +1,8 @@
 xcopy ..\vortexcore\bin\debug\*.dll .\bin\debug /I /D /Y
 xcopy ..\vortexcore\bin\debug\*.pdb .\bin\debug /I /D /Y
 
-xcopy ..\vortexcore\bin\release\*.dll .\bin\debug /I /D /Y
-xcopy ..\vortexcore\bin\release\*.pdb .\bin\debug /I /D /Y
+xcopy ..\vortexcore\bin\release\*.dll .\bin\release /I /D /Y
+xcopy ..\vortexcore\bin\release\*.pdb .\bin\release /I /D /Y
 
 
  
