cb970f1328a3330610909b0ec1edb15759f778a3
voidstorm <flo.putz@gmail.com>
2017-05-18 23:22:33 +0200

    added vkinstance

--- vortexcore/include/VortexCore/private/RenderContextVulkan.h
+++ vortexcore/include/VortexCore/private/RenderContextVulkan.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <memory>
+#include <atomic>
 #include "vulkan\vulkan.h"
 
 
@@ -7,17 +8,39 @@ namespace Vt {
   namespace Gfx {
     class RenderContextVulkan {
 
-      RenderContextVulkan() {
-
-      }
-
-      ~RenderContextVulkan() {
-
-      }
-
       friend class RenderContext;
       friend class std::unique_ptr<RenderContextVulkan>;
       friend struct std::unique_ptr<RenderContextVulkan>::deleter_type;
+
+      RenderContextVulkan(){
+        mVkInstance = getVkInstance();
+        mInstanceCount++;
+      }
+
+      ~RenderContextVulkan() {
+        mInstanceCount--;
+        if (mInstanceCount == 0) {
+          //release vulkan instance and device
+        }
+      }
+
+      VkInstance getVkInstance() {
+        auto create_instance = [this]()->VkInstance {
+
+        };
+        static VkInstance vk_instance = create_instance();
+        return vk_instance;
+      }
+
+
+
+      VkInstance  mVkInstance= nullptr;
+      VkDevice    mVkDevice= nullptr;
+
+      static std::atomic<unsigned> mInstanceCount;
     };
+
+    std::atomic<unsigned> RenderContextVulkan::mInstanceCount= 0;
+
   }
 }
